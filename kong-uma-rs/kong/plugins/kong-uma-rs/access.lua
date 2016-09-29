4be8b5030079db1ee3cd215e91c73367e7af9f1b
yuriyz <yzabrovarniy@gmail.com>
2016-09-29 11:00:38 +0300

    conf print

--- kong-uma-rs/kong/plugins/kong-uma-rs/access.lua
+++ kong-uma-rs/kong/plugins/kong-uma-rs/access.lua
@@ -1,7 +1,12 @@
 local _M = {}
 
 function _M.execute(conf)
-  ngx.log(ngx.DEBUG, "KONG-UMA: access")
+  ngx.log(ngx.DEBUG, "KONG-UMA: access, conf: " .. conf.oxd_host)
+  ngx.log(ngx.DEBUG, "KONG-UMA: access, conf: " .. conf.oxd_port)
+  ngx.log(ngx.DEBUG, "KONG-UMA: access, conf: " .. conf.uma_server_host)
+  ngx.log(ngx.DEBUG, "KONG-UMA: access, conf: " .. conf.protection_document)
+
+
 end
 
 return _M
