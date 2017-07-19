199cc36ee0c3c5e20357f834bbd88d89673a5584
Meghna Joshi <ldeveloperl1985@gmail.com>
2017-07-19 16:51:37 +0530

    Set header in authentication response

--- kong-openid-rp/kong/plugins/kong-openid-rp/access.lua
+++ kong-openid-rp/kong/plugins/kong-openid-rp/access.lua
@@ -3,6 +3,7 @@ local responses = require "kong.tools.responses"
 local stringy = require "stringy"
 local singletons = require "kong.singletons"
 local cache = require "kong.tools.database_cache"
+local constants = require "kong.constants"
 
 local function isempty(s)
     return s == nil or s == ''
@@ -29,6 +30,31 @@ local function load_oxd_by_oxd_id(oxd_id)
     return creds[1]
 end
 
+local function load_consumer(consumer_id, anonymous)
+    local result, err = singletons.dao.consumers:find { id = consumer_id }
+    if not result then
+        if anonymous and not err then
+            err = 'anonymous consumer "'..consumer_id..'" not found'
+        end
+        return nil, err
+    end
+    return result
+end
+
+local function set_consumer(consumer, credential)
+    ngx.header[constants.HEADERS.CONSUMER_ID] = consumer.id
+    ngx.header[constants.HEADERS.CONSUMER_CUSTOM_ID] = consumer.custom_id
+    ngx.header[constants.HEADERS.CONSUMER_USERNAME] = consumer.username
+    ngx.ctx.authenticated_consumer = consumer
+    if credential then
+        ngx.header["X-OXD-ID"] = credential.oxd_id
+        ngx.ctx.authenticated_credential = credential
+        ngx.header[constants.HEADERS.ANONYMOUS] = nil -- in case of auth plugins concatenation
+    else
+        ngx.header[constants.HEADERS.ANONYMOUS] = true
+    end
+end
+
 function _M.execute(conf)
     local httpMethod = ngx.req.get_method()
     local authorization_code = ngx.req.get_headers()["authorization_code"]
@@ -38,13 +64,13 @@ function _M.execute(conf)
 
     -- ------- validation ------
     if isempty(authorization_code) then
-        return responses.send_HTTP_BAD_REQUEST("Please pass authorization code")
+        return responses.send_HTTP_BAD_REQUEST("authorization_code is required")
     end
     if isempty(state) then
-        return responses.send_HTTP_BAD_REQUEST("Please pass sate")
+        return responses.send_HTTP_BAD_REQUEST("state is required")
     end
     if isempty(oxd_id) then
-        return responses.send_HTTP_BAD_REQUEST("Please pass oxd_id")
+        return responses.send_HTTP_BAD_REQUEST("oxd_id is required")
     end
     ngx.log(ngx.DEBUG, "kong-openid-rp : Access - http_method: " .. httpMethod .. ", code: " .. authorization_code .. ", path: " .. path .. ", state: " .. state)
     -- ------------------------
@@ -63,14 +89,22 @@ function _M.execute(conf)
         return responses.send_HTTP_UNAUTHORIZED("Unauthorized")
     end
 
+    -- retrieve the consumer linked to this API key, to set appropriate headers
+    local consumer, err = cache.get_or_set(cache.consumer_key(oxdConfig.consumer_id),
+        nil, load_consumer, oxdConfig.consumer_id)
+
+    if err then
+        return responses.send_HTTP_INTERNAL_SERVER_ERROR(err)
+    end
+
     if response["status"] == "ok" then
-        return -- ACCESS GRANTED
+        set_consumer(consumer, oxdConfig)
+        return true -- ACCESS GRANTED
     else
         return responses.send_HTTP_UNAUTHORIZED("Unauthorized")
     end
 
-    return responses.send_HTTP_FORBIDDEN("Unknown (unsupported) status code from oxd server for uma_rs_check_access operation.")
+    return responses.send_HTTP_FORBIDDEN("Unknown (unsupported) status code from oxd server for opendid coonect oauth operation.")
 end
 
 return _M
-
