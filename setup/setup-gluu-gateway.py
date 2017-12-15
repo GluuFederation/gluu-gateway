fc545c6b4ae05a384eb31285d813c50bc41af22a
Meghna Joshi <ldeveloperl1985@gmail.com>
2017-12-15 16:04:27 +0530

    Updated setup script by adding Gluu license

--- setup/setup-gluu-gateway.py
+++ setup/setup-gluu-gateway.py
@@ -451,8 +451,34 @@ if __name__ == "__main__":
 
         print cnf
         proceed = kongSetup.makeBoolean(kongSetup.getPrompt('Proceed with these values(Y|n)', 'Y'))
+        licence = False
 
         if proceed:
+            msg = "------------------------------------------------------------------------------------- \n" \
+                  + "The Gluu Support License (GLUU-SUPPORT) \n\n" \
+                  + "Copyright (c) 2017 Gluu \n\n" \
+                  + "Permission is hereby granted to any person obtaining a copy \n" \
+                  + "of this software and associated documentation files (the 'Software'), to deal \n" \
+                  + "in the Software without restriction, including without limitation the rights \n" \
+                  + "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell \n" \
+                  + "copies of the Software, and to permit persons to whom the Software is \n" \
+                  + "furnished to do so, subject to the following conditions: \n\n" \
+                  + "The above copyright notice and this permission notice shall be included in all \n" \
+                  + "copies or substantial portions of the Software. \n\n" \
+                  + "The person using this software has an active support subscription while the software \n" \
+                  + "is in use. \n\n" \
+                  + "THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR \n" \
+                  + "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, \n" \
+                  + "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE \n" \
+                  + "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER \n" \
+                  + "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, \n" \
+                  + "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE \n" \
+                  + "SOFTWARE. \n" \
+                  + "------------------------------------------------------------------------------------- \n"
+            print msg
+            licence = kongSetup.makeBoolean(kongSetup.getPrompt('Do you acknowledge that use of the Gluu Gateway is under the GLUU-SUPPORT license?(y|N)', 'N'))
+
+        if proceed and licence:
             kongSetup.genKongSslCertificate()
             kongSetup.configurePostgres()
             kongSetup.configureOxd()
