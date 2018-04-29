6054c5d340e4913a18568ad802591ff7b28f13c7
mk <mk@gluu.org>
2018-04-29 18:09:35 +1000

    Katalon test a_Allow

--- tests/katalon/Gluu Gateway/Object Repository/Page_oxAuth/a_Allow.rs
+++ tests/katalon/Gluu Gateway/Object Repository/Page_oxAuth/a_Allow.rs
@@ -4,24 +4,30 @@
    <name>a_Allow</name>
    <tag></tag>
    <elementGuidId>70310bbb-62b6-4649-900a-f1390b5f86b8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>id(&quot;authorizeForm:allowButton&quot;)</value>
+      </entry>
+   </selectorCollection>
    <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <webElementProperties>
-      <isSelected>true</isSelected>
+      <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>tag</name>
       <type>Main</type>
       <value>a</value>
    </webElementProperties>
    <webElementProperties>
-      <isSelected>true</isSelected>
+      <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>id</name>
       <type>Main</type>
       <value>authorizeForm:allowButton</value>
    </webElementProperties>
    <webElementProperties>
-      <isSelected>true</isSelected>
+      <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>href</name>
       <type>Main</type>
@@ -42,14 +48,14 @@
       <value>button button-primary</value>
    </webElementProperties>
    <webElementProperties>
-      <isSelected>true</isSelected>
+      <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>text</name>
       <type>Main</type>
       <value>Allow</value>
    </webElementProperties>
    <webElementProperties>
-      <isSelected>false</isSelected>
+      <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
