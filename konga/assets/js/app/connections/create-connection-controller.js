895784351a9bec3897b3509ddbacb244d63dfc4a
Meghna Joshi <ldeveloperl1985@gmail.com>
2018-05-09 16:02:48 +0530

    Added informative message #99

--- konga/assets/js/app/connections/create-connection-controller.js
+++ konga/assets/js/app/connections/create-connection-controller.js
@@ -3,47 +3,53 @@
  *
  * Note that this file should only contain controllers and nothing else.
  */
-(function() {
-    'use strict';
+(function () {
+  'use strict';
 
-    angular.module('frontend.connections')
-        .controller('CreateConnectionController', [
-            '$scope','$rootScope','$log','NodeModel',
-            'MessageService','SettingsService','$uibModalInstance',
-            function controller($scope,$rootScope,$log,NodeModel,
-                                MessageService,SettingsService,$uibModalInstance) {
+  angular.module('frontend.connections')
+    .controller('CreateConnectionController', [
+      '$scope', '$rootScope', '$log', 'NodeModel',
+      'MessageService', 'SettingsService', '$uibModalInstance',
+      function controller($scope, $rootScope, $log, NodeModel,
+                          MessageService, SettingsService, $uibModalInstance) {
 
-                $scope.kong_versions = SettingsService.getKongVersions()
+        $scope.kong_versions = SettingsService.getKongVersions()
 
-                $scope.node = {
-                    kong_admin_url : '',
-                    kong_version : '0-11-x',
+        $scope.node = {
+          kong_admin_url: '',
+          kong_version: '0-11-x',
+        }
+
+        $scope.close = function () {
+          $uibModalInstance.dismiss()
+        }
+
+        $scope.create = function () {
+          $scope.busy = true;
+          NodeModel
+            .create(angular.copy($scope.node))
+            .then(
+              function onSuccess(result) {
+                $log.info('New node created successfully', result)
+                MessageService.success('New node created successfully');
+                $scope.busy = false;
+                $rootScope.$broadcast('kong.node.created', result.data)
+                $uibModalInstance.dismiss()
+              }, function (err) {
+                $scope.busy = false
+                NodeModel.handleError($scope, err)
+                if ($scope.errors.kong_admin_url) {
+                  $scope.errors.kong_admin_url = "Kong Admin URL is required."
                 }
-
-                $scope.close = function(){
-                    $uibModalInstance.dismiss()
+                if ($scope.errors.name) {
+                  $scope.errors.name = "Name is required."
                 }
+              }
+            )
+          ;
+        }
 
-                $scope.create = function() {
-                    $scope.busy = true;
-                    NodeModel
-                        .create(angular.copy($scope.node))
-                        .then(
-                            function onSuccess(result) {
-                                $log.info('New node created successfully',result)
-                                MessageService.success('New node created successfully');
-                                $scope.busy = false;
-                                $rootScope.$broadcast('kong.node.created',result.data)
-                                $uibModalInstance.dismiss()
-                            },function(err){
-                                $scope.busy = false
-                                NodeModel.handleError($scope,err)
-                            }
-                        )
-                    ;
-                }
-
-            }
-        ])
-    ;
+      }
+    ])
+  ;
 }());
