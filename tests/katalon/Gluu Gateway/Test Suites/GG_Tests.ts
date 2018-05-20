50b0de6a67c30138e1d607e9f0c1611d65c135e9
Natalia Glina <natalia.glina@sagiton.pl>
2018-05-21 09:09:33 +1000

    Fixed the flow of UMA Mode

--- tests/katalon/Gluu Gateway/Test Suites/GG_Tests.ts
+++ tests/katalon/Gluu Gateway/Test Suites/GG_Tests.ts
@@ -4,7 +4,7 @@
    <name>GG_Tests</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-05-14T13:49:37</lastRun>
+   <lastRun>2018-05-21T08:59:19</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
