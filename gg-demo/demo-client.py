91e7bf52f7a3d0e397ef5276995fa8339d7676a3
michal kepkowski <mk@gluu.org>
2018-06-20 22:15:25 +1000

    demo

--- gg-demo/demo-client.py
+++ gg-demo/demo-client.py
@@ -1,105 +1,28 @@
 #!/usr/bin/python
-
-import requests
-import cgi
 import cgitb; cgitb.enable()
-
-print "HTTP/1.0 200 OK"
-print "Content-type: text/html\n\n"
-print ""
-
-api_url="https://kong.example.com/demo/protected1"
-gg_base_url = "http://demo.gluu.org"
-oxd_host = "https://demo.gluu.org"
-ce_url="https://demo.gluu.org"
-api_path = "posts"
-
-host_with_claims="gathering.example.com"
-host_without_claims="non-gathering.example.com"
-host=""
-
-client_oxd_id="a5c511db-bbec-4d06-a046-b5feef7658a8"
-client_id="@!7A1F.7A69.7E9A.EFBA!0001!AD32.2532!0008!893E.3C4C.D852.8BB5"
-client_secret="ea5c3f6a-58f4-4aba-bead-4b36f0c0f520"
-
-claims_redirect_url="http://demo.gluu.org:8080/cgi-bin/demo-client.cgi"
+from calls import *
+from config import *
 
 
+def handleClaimsGatheringResponse():
+    if (isTicketInUrl()):
+        arguments = cgi.FieldStorage()
+        ticket = arguments['ticket'].value
+        access_token = callOxdToGetClientAccessToken(url=oxd_host + ":8443/get-client-token")
 
-def callAPIwithoutRPT (url, host):
-    response = requests.get(url, headers={"Host": host})
-    print "Client calls API without RPT token</br>"
-    print url
-    print "Response status: "+str(response.status_code)+"</br>"
-    print "Response headers: "+str(response.headers)+"</br>"
-    return response.headers["WWW-Authenticate"].split(",")[3].split("=")[1].replace("\"", "")
-
-def callAPIwithRPT (url, host, rpt):
-    response = requests.get(url, headers={"Host": host, "Authorization":"Bearer "+rpt})
-    print "Client calls API with RPT token"+"</br>"
-    print url+"</br>"
-    print "Response body: </br>"+str(response.json())+"</br>"
-
-def callOxdToGetClientAccessToken(url):
-    print "Authenticating client in oxd-server"
-    response = requests.post(url,
-                             headers={"Content-Type": "application/json"},
-                             json={"oxd_id": client_oxd_id,
-                                   "client_id": client_id,
-                                   "client_secret": client_secret,
-                                   "op_host": ce_url,
-                                   "scope": ["uma_protection","openid"]},
-                             verify=False)
-    return response.json()['data']['access_token']
+        # Here is my PCT token!
+        # Client attempts to get RPT at UMA /token endpoint, this time presenting the PCT
+        displayActionName(" Client attempts to get RPT at UMA /token endpoint, this time presenting the PCT")
+        need_info, token, redirect_url = callOxdToGetRpt(access_token, ticket, url=oxd_host + ":8443/uma-rp-get-rpt",
+                                                         scope="demo_scope_gathering")
+        callAPIwithRPT(url=gg_base_url + ":8000/" + api_path, host=host_with_claims, rpt=token)
+        quit()
 
 
-def callOxdToGetRpt(access_token, ticket, url, scope):
-    print "Client calls AS UMA /token endpoint with permission ticket and client creds"
-    response = requests.post(url,
-                             headers={"Content-Type": "application/json",
-                                      "Authorization": "Bearer " + access_token},
-                             json={"oxd_id": client_oxd_id,
-                                   "ticket": ticket,
-                                   "scope": [scope,"uma_protection"]},
-                             verify=False)
-    if(response.json()['status'] == "error"):
-        return True,"", response.json()['data']['details']['redirect_user']
-    else :
-        return False,response.json()['data']['access_token'],""
-
-def isTicketInUrl():
-    arguments = cgi.FieldStorage()
-    return 'ticket' in arguments
-
-def isClaimInUrl():
-    arguments = cgi.FieldStorage()
-    if('claim' in arguments):
-        return host_with_claims, "demo_scope_gathering"
-    else:
-        return host_without_claims, "demo_scope_non_gathering"
-
-
-
-
-
-
-
-
-
+displayPageHeader()
 host,scope = isClaimInUrl()
+handleClaimsGatheringResponse()
 
-if(isTicketInUrl()):
-    arguments = cgi.FieldStorage()
-    ticket = arguments['ticket'].value
-    access_token = callOxdToGetClientAccessToken(url=oxd_host + ":8443/get-client-token")
-
-    # Here is my PCT token!
-    # Client attempts to get RPT at UMA /token endpoint, this time presenting the
-    # PCT
-    need_info, token, redirect_url = callOxdToGetRpt(access_token, ticket, url=oxd_host + ":8443/uma-rp-get-rpt",scope="demo_scope_gathering")
-    print "</br>RPT: "+token+"</br>"
-    callAPIwithRPT(url=gg_base_url + ":8000/" + api_path, host=host_with_claims, rpt=token)
-    quit()
 
 # Client calls API without RPT token
 ticket = callAPIwithoutRPT( url=gg_base_url+":8000/"+api_path, host=host)
@@ -118,7 +41,6 @@ if(not need_info):
 # AS returns needs_info with clailms gathering URI, which the user should
 # put in his browser. Link shortner would be nice if the user has to type it in.
 if(need_info):
-    fullClaimRedirectUrl = redirect_url+"&claims_redirect_uri="+claims_redirect_url
-    print "</br><a href="+fullClaimRedirectUrl+">"+fullClaimRedirectUrl+"</a></br>"
-
-
+    fullClaimRedirectUrl = createClaimsUrl(redirect_url,claims_redirect_url)
+    displayActionName("4. Claims gathering url")
+    displayPanel("<a href="+fullClaimRedirectUrl+">"+fullClaimRedirectUrl+"</a>")
---
+++ gg-demo/display.py
@@ -0,0 +1,35 @@
+import json
+
+def displayPageHeader():
+    print "HTTP/1.0 200 OK"
+    print "Content-type: text/html\n\n"
+    print "<head>"
+    print "<title>Gluu Gateway Demo</title>"
+    print "<meta charset=\"utf-8\">"
+    print "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+    print "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">"
+    print "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>"
+    print "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>"
+    print "</head>"
+    print "<body style=\"top: 8rem;position: absolute;\">"
+    print "<header style=\"position: fixed;top: 0; right: 0;left: 0;height: 6.6rem;background-color: #223243;color: #fff;\">"
+    print "<img style=\"display: inline-block;\" src='/resource/gluu-gateway.png' />"
+    print "<h3 style=\"display: inline-block; float:right;\">DEMO</h3>"
+    print "</header>"
+
+def displayResponse(response):
+    body = "<table style=\"width: 100%; table-layout: fixed\"><tr><td width=\"200px\">Request url:</td><td>" + response.url + "</td></tr>"
+    body += "<tr><td>Request headers:</td><td>" + str(response.request.headers) + "</td></tr>"
+    body += "<tr><td>Response status:</td><td>" + str(response.status_code) + "</td></tr>"
+    body += "<tr><td>Response headers:</td><td>" + str(response.headers) + "</td></tr>"
+    body += "<tr><td>Response body:</td><td> <pre>" + json.dumps(response.json(),indent=4) + "</pre></td></tr></table>"
+    displayPanel(body)
+
+def displayActionName(name):
+    print("<h3 style=\"padding-left:20px;\">"+name+"</h3></br>")
+
+
+def displayPanel(body):
+    print "<div style=\"padding: 20px; margin:20px;\" class=\" panel panel-default\">"
+    print body
+    print "</div>"
