798bebd61753236f2a5facb2820903424c893bf0
Natalia Glina <natalia.glina@sagiton.pl>
2018-06-22 12:29:26 +1000

    Changed xpaths after UI change

--- tests/katalon/Gluu Gateway/Test Suites/GG_Tests.ts
+++ tests/katalon/Gluu Gateway/Test Suites/GG_Tests.ts
@@ -4,7 +4,7 @@
    <name>GG_Tests</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-05-27T20:26:17</lastRun>
+   <lastRun>2018-06-22T12:03:43</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
