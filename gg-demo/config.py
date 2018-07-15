82c4c0b48ebb6372f0a5fc82fdbe7d6071a331a4
Mike Schwartz <mike@gluu.org>
2018-07-15 11:28:30 -0500

    fix

--- gg-demo/config.py
+++ gg-demo/config.py
@@ -8,9 +8,9 @@ api_path = "posts"
 host_with_claims="gathering.example.com"
 host_without_claims="non-gathering.example.com"
 
-client_oxd_id="b7581ab4-8d79-4726-b122-77c0169e0a11"
-client_id="@!7A1F.7A69.7E9A.EFBA!0001!AD32.2532!0008!5245.0911.0853.050A"
-client_secret="09e59ea4-b74a-4796-8972-71e6c0af0a41"
+client_oxd_id=""
+client_id=""
+client_secret=""
 
 claims_redirect_url="http://demo.gluu.org:8080/cgi-bin/demo-client.cgi"
 
