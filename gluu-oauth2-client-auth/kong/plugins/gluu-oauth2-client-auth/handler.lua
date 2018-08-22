8de15e76cd3fae7edb3c497498b3890607c2fd5a
Alexander Altshuler <altexy@gmail.com>
2018-08-22 17:39:23 +0300

    remove obsolete files, minot handler.lua fixes

--- gluu-oauth2-client-auth/kong/plugins/gluu-oauth2-client-auth/handler.lua
+++ gluu-oauth2-client-auth/kong/plugins/gluu-oauth2-client-auth/handler.lua
@@ -2,13 +2,13 @@ local BasePlugin = require "kong.plugins.base_plugin"
 local access = require "kong.plugins.gluu-oauth2-client-auth.access"
 
 local handler = BasePlugin:extend()
--- handler.priority = 999
+handler.priority = 999
 
 -- Your plugin handler's constructor. If you are extending the
 -- Base Plugin handler, it's only role is to instanciate itself
 -- with a name. The name is your plugin name as it will be printed in the logs.
 function handler:new()
-    handler.super.new(self, "demo")
+    handler.super.new(self, "gluu-oauth2-client-auth")
 end
 
 function handler:access(config)
