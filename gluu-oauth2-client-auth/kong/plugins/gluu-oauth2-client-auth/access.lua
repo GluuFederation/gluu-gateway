8d342705d70ba69585cc708cf382ca85914b9d2b
Meghna Joshi <ldeveloperl1985@gmail.com>
2018-08-28 18:18:32 +0530

    Refactor OAuth plugin with anonymous, hide_credential feature #197

--- gluu-oauth2-client-auth/kong/plugins/gluu-oauth2-client-auth/access.lua
+++ gluu-oauth2-client-auth/kong/plugins/gluu-oauth2-client-auth/access.lua
@@ -1,5 +1,5 @@
+local constants = require "kong.constants"
 local oxd = require "oxdweb"
-
 -- we don't store our token in lrucache - we don't want it be pushed out
 local access_token
 local access_token_expire = 0
@@ -7,55 +7,90 @@ local EXPIRE_DELTA = 10
 
 local PLUGINNAME = "gluu-oauth2-client-auth"
 
-
 local lrucache = require "resty.lrucache"
 -- it can be shared by all the requests served by each nginx worker process:
-local token_cache, err = lrucache.new(1000)  -- allow up to 1000 items in the cache
+local token_cache, err = lrucache.new(1000) -- allow up to 1000 items in the cache
 if not token_cache then
     return error("failed to create the cache: " .. (err or "unknown"))
 end
 
-return function(conf)
+local function load_consumer(consumer_id, anonymous)
+    local result, err = kong.db.consumers:select({ id = consumer_id })
+    if not result then
+        if anonymous and not err then
+            err = 'anonymous consumer "' .. consumer_id .. '" not found'
+        end
+
+        return nil, err
+    end
+
+    return result
+end
+
+local function set_consumer(body)
+    local const = constants.HEADERS
+    local new_headers = {
+        [const.CONSUMER_ID] = body.consumer.id,
+        [const.CONSUMER_CUSTOM_ID] = tostring(body.consumer.custom_id),
+        [const.CONSUMER_USERNAME] = tostring(body.consumer.username),
+        ["X-OAuth-Client-ID"] = tostring(body.client_id),
+        ["X-OAuth-Expiration"] = tostring(body.exp)
+    }
+    kong.service.request.set_headers(new_headers)
+end
+
+local function get_token(conf)
     local authorization = ngx.var.http_authorization
-    local token
     if authorization and #authorization > 0 then
         local from, to, err = ngx.re.find(authorization, "\\s*[Bb]earer\\s+(.+)", "jo", nil, 1)
         if from then
-            token = authorization:sub(from, to)
+            return authorization:sub(from, to) -- Return token
         end
         if err then
             kong.log.err(err)
+            return kong.response.exit(500, { message = "Failed to get token from header" })
         end
     end
 
+    -- Hide credentials
+    kong.log.debug("hide_credentials: ", conf.hide_credentials)
+    if conf.hide_credentials then
+        kong.log.debug("Hide authorization header")
+        kong.service.request.clear_header(authorization)
+    end
+
+    return nil
+end
+
+return function(conf)
+    local token = get_token(conf)
     if not token then
-        kong.log("No token")
-        return kong.response.exit(401)
+        kong.log.err("Token not found")
+        return kong.response.exit(401, { message = "Token not found" })
     end
 
     local now = ngx.now()
-    print(now)
-    print(access_token_expire)
-    if not access_token  or access_token_expire < now + EXPIRE_DELTA then
+    kong.log.debug("Current datetime: ", now)
+    kong.log.debug("access_token_expire: ", access_token_expire)
+    if not access_token or access_token_expire < now + EXPIRE_DELTA then
         access_token_expire = access_token_expire + EXPIRE_DELTA -- avoid multiple token requests
-        local response = oxd.get_client_token(
-            conf.oxd_http_url,
+        local response = oxd.get_client_token(conf.oxd_url,
             {
                 client_id = conf.client_id,
                 client_secret = conf.client_secret,
                 scope = "openid profile email",
-                op_host = conf.op_server,
-            }
-        )
+                op_host = conf.op_url,
+            })
 
         local status = response.status
         local body = response.body
 
+        kong.log.err("Protection access token -- status: ", status)
         if status >= 300 or not body.access_token then
             access_token = nil
             access_token_expire = 0
-            kong.log.err("Failed to get access token")
-            return kong.response.exit(500)
+            kong.log.err("Failed to get access token.")
+            return kong.response.exit(status, { message = "Failed to get access token" })
         end
 
         access_token = body.access_token
@@ -68,61 +103,77 @@ return function(conf)
     end
 
     local body, stale_data = token_cache:get(token)
-    if not body or stale_data then
 
-        local response = oxd.introspect_access_token(
-            conf.oxd_http_url,
-            {
-                oxd_id = conf.oxd_id,
-                access_token = token,
-            },
-            access_token
-        )
-
-        local status = response.status
-
-        if status >= 300 then
-            kong.log.err("TODO")
-            -- should we cache negative resposes? https://github.com/GluuFederation/gluu-gateway/issues/213
-
-            -- TODO IMO we should check for conf.ananimouse here
-
-            -- TODO should we distinguish between unexected errors and not valid credentials?
-            return kong.response.exit(403)
-        end
-
-        body = response.body
-
-        print("calling select_by_custom_id: ", body.client_id)
-        -- TODO implement consumer detection
-        local consumer, err = kong.db.consumers:select_by_custom_id(body.client_id)
-        print"after select_by_custom_id"
-        if err then
-            kong.log.err("select_by_custom_id error: ", err)
-        end
-        if not consumer then
-            print("consumer not found")
-            return kong.response.exit(401)
-        end
-
-        -- cache consumer id also, avoid DB call on every request
-        body.consumer = consumer
-
-        if body.exp and body.iat then
-            token_cache:set(token, body, body.iat + body.exp - EXPIRE_DELTA)
-        else
-            kong.log.err(PLUGINNAME .. ": missed exp or iat fields")
-            -- TODO what we must do?
-        end
+    if body and not stale_data then
+        -- we're already authenticated
+        kong.log.debug("Token cache found. we're already authenticated")
+        set_consumer(body)
+        return
     end
 
-    print("Consumer: ", body.consumer.id)
+    kong.log.debug("Token cache not found.")
+    local response = oxd.introspect_access_token(conf.oxd_url,
+        {
+            oxd_id = conf.oxd_id,
+            access_token = token,
+        },
+        access_token)
+    local status = response.status
+
+    if status >= 300 then
+        kong.log.err("TODO")
+        -- should we cache negative resposes? https://github.com/GluuFederation/gluu-gateway/issues/213
+
+        -- Check for conf.ananimouse here
+        if conf.anonymous ~= "" then
+            -- get anonymous user
+            local consumer_cache_key = kong.db.consumers:cache_key(conf.anonymous)
+            local consumer, err = kong.cache:get(consumer_cache_key, nil, load_consumer, conf.anonymous, true)
+
+            if err then
+                kong.log.err(err)
+                return kong.response.exit(500, { message = "An unexpected error ocurred" })
+            end
+            set_consumer({ consumer })
+            return
+        end
+
+        -- TODO should we distinguish between unexected errors and not valid credentials?
+        return kong.response.exit(401, { message = "Token expired" })
+    end
+
+    body = response.body
+
+    kong.log.debug("calling select_by_custom_id: ", body.client_id)
+
+    -- TODO implement consumer detection
+    local consumer, err = kong.db.consumers:select_by_custom_id(body.client_id)
+    kong.log.debug("after select_by_custom_id")
+
+    if err then
+        kong.log.err("select_by_custom_id error: ", err)
+        return kong.response.exit(500, { message = "An unexpected error ocurred" })
+    end
+
+    if not consumer then
+        kong.log.debug("consumer not found")
+        return kong.response.exit(401, { message = "Consumer not found" })
+    end
+
+    -- cache consumer id also, avoid DB call on every request
+    body.consumer = consumer
+
+    if body.exp and body.iat then
+        token_cache:set(token, body, body.exp - body.iat)
+    else
+        kong.log.err(PLUGINNAME .. ": missed exp or iat fields")
+        return kong.response.exit(500, { message = "EXP and IAT fileds missing in introspection response" })
+    end
+
+    kong.log.debug("Consumer: ", body.consumer.id)
 
     -- TODO implement scope expressions, id any
 
     -- TODO set headers
-    kong.service.request.set_header("X-Consumer-ID", body.consumer.id)
-    kong.service.request.set_header("X-Consumer-Custom-ID", body.client_id)
-    kong.service.request.set_header("X-OAuth-Client-ID", body.client_id)
-    kong.service.request.set_header("X-OAuth-Expiration", tostring(math.floor(body.iat + body.exp)))
+    set_consumer(body)
 end
--- t/specs/gluu-oauth2-client-auth/test_spec.lua
+++ t/specs/gluu-oauth2-client-auth/test_spec.lua
@@ -1,28 +1,28 @@
-local utils = require"test_utils"
+local utils = require "test_utils"
 local sh, stdout, stderr, sleep, sh_ex, sh_until_ok =
-    utils.sh, utils.stdout, utils.stderr, utils.sleep, utils.sh_ex, utils.sh_until_ok
+utils.sh, utils.stdout, utils.stderr, utils.sleep, utils.sh_ex, utils.sh_until_ok
 
-local kong_utils = require"kong_utils"
-local JSON = require"JSON"
+local kong_utils = require "kong_utils"
+local JSON = require "JSON"
 
-local host_git_root = os.getenv"HOST_GIT_ROOT"
-local git_root = os.getenv"GIT_ROOT"
+local host_git_root = os.getenv "HOST_GIT_ROOT"
+local git_root = os.getenv "GIT_ROOT"
 local test_root = host_git_root .. "/t/specs/gluu-oauth2-client-auth"
 
-test("Simple oxd Kong plugin test", function()
+describe("Simple oxd Kong plugin test", function()
     _G.ctx = {}
     local ctx = _G.ctx
     ctx.finalizeres = {}
     ctx.host_git_root = host_git_root
 
     local print_logs = true
-    finally(function()
+    teardown(function()
         if print_logs then
             if ctx.kong_id then
                 sh("docker logs ", ctx.kong_id, " || true") -- don't fail
             end
             if ctx.oxd_id then
-                sh("docker logs ", ctx.oxd_id, " || true")  -- don't fail
+                sh("docker logs ", ctx.oxd_id, " || true") -- don't fail
             end
         end
 
@@ -33,9 +33,8 @@ test("Simple oxd Kong plugin test", function()
         end
     end)
 
-
     kong_utils.docker_unique_network()
-    kong_utils.kong_postgress_custom_plugins{
+    kong_utils.kong_postgress_custom_plugins {
         plugins = {
             ["gluu-oauth2-client-auth"] = host_git_root .. "/gluu-oauth2-client-auth/kong/plugins/gluu-oauth2-client-auth",
         },
@@ -48,128 +47,141 @@ test("Simple oxd Kong plugin test", function()
     kong_utils.backend()
     kong_utils.oxd_mock(test_root .. "/oxd-model.lua")
 
-    print"=============== Create a Sevice"
-    local res, err = sh_until_ok(10,
-        [[curl --fail -i -sS -X POST --url http://localhost:]],
-        ctx.kong_admin_port, [[/services/ --data 'name=demo-service' --data 'url=http://backend']]
-    )
+    local access_token, register_site_response
 
-    print"=============== Create a Route"
-    local res, err = sh_until_ok(10,
-        [[curl --fail -i -sS -X POST  --url http://localhost:]],
-        ctx.kong_admin_port, [[/services/demo-service/routes --data 'hosts[]=backend.com']]
-    )
+    setup(function()
+        print "---------------- Setup test"
 
-    print"=============== Test it works"
-    local res, err = sh_until_ok(10, [[curl --fail -i -sS -X GET --url http://localhost:]],
-        ctx.kong_proxy_port, [[/ --header 'Host: backend.com']])
+        print "Create a Sevice"
+        local res, err = sh_until_ok(10,
+            [[curl --fail -i -sS -X POST --url http://localhost:]],
+            ctx.kong_admin_port, [[/services/ --data 'name=demo-service' --data 'url=http://backend']])
 
+        print "Create a Route"
+        local res, err = sh_until_ok(10,
+            [[curl --fail -i -sS -X POST  --url http://localhost:]],
+            ctx.kong_admin_port, [[/services/demo-service/routes --data 'hosts[]=backend.com']])
 
-    local register_site = {
-        scope = { "openid", "uma_protection" },
-        op_host = "just_stub",
-        authorization_redirect_uri = "https://client.example.com/cb",
-        client_name = "demo plugin",
-        grant_types = { "client_credentials" }
-    }
-    local register_site_json = JSON:encode(register_site)
+        local register_site = {
+            scope = { "openid", "uma_protection" },
+            op_host = "just_stub",
+            authorization_redirect_uri = "https://client.example.com/cb",
+            client_name = "demo plugin",
+            grant_types = { "client_credentials" }
+        }
+        local register_site_json = JSON:encode(register_site)
 
-    local res, err = sh_ex(
-        [[curl --fail -v -sS -X POST --url http://localhost:]], ctx.oxd_port,
-        [[/register-site --header 'Content-Type: application/json' --data ']],
-        register_site_json, [[']]
-    )
-    local register_site_response = JSON:decode(res)
+        local res, err = sh_ex([[curl --fail -v -sS -X POST --url http://localhost:]], ctx.oxd_port,
+            [[/register-site --header 'Content-Type: application/json' --data ']],
+            register_site_json, [[']])
+        register_site_response = JSON:decode(res)
 
-    local get_client_token = {
-        op_host = "just_stub",
-        client_id = register_site_response.client_id,
-        client_secret = register_site_response.client_secret,
-    }
-    local get_client_token_json = JSON:encode(get_client_token)
-    local res, err = sh_ex(
-        [[curl --fail -v -sS -X POST --url http://localhost:]], ctx.oxd_port,
-        [[/get-client-token --header 'Content-Type: application/json' --data ']],
-        get_client_token_json, [[']]
-    )
+        local get_client_token = {
+            op_host = "just_stub",
+            client_id = register_site_response.client_id,
+            client_secret = register_site_response.client_secret,
+        }
+        local get_client_token_json = JSON:encode(get_client_token)
+        local res, err = sh_ex([[curl --fail -v -sS -X POST --url http://localhost:]], ctx.oxd_port,
+            [[/get-client-token --header 'Content-Type: application/json' --data ']],
+            get_client_token_json, [[']])
 
-    local response = JSON:decode(res)
+        local response = JSON:decode(res)
 
-    local access_token = response.access_token
+        access_token = response.access_token
+    end)
 
-    print"=============== Test validation on plugin config"
-    local res, err = sh_ex(
-        [[curl -i -sS -X POST  --url http://localhost:]], ctx.kong_admin_port,
-        [[/services/demo-service/plugins/ --data 'name=gluu-oauth2-client-auth' ]],
-        [[ --data "config.op_url=stub" ]],
-        [[ --data "config.oxd_url=http://oxd-mock" ]],
-        [[ --data "config.anonymous=123-456" ]],
-        [[ --data "config.oauth_scope_expression=[{\"path\":\"/posts\",\"conditions\":[{\"httpMethods\":[\"GET\",\"DELETE\",\"POST\",\"scope_expression\":{\"and\":[\"admin\",{\"not\":[\"employee\"]}]}}]}]" ]],
-        [[ --data "config.client_id=]], register_site_response.client_id, "\" ",
-        [[ --data "config.client_secret=]], register_site_response.client_secret, "\" ",
-        [[ --data "config.oxd_id=]], register_site_response.oxd_id, "\" "
-    )
-    assert(res:find("400"), 1, true)
+    describe("Test proxy access point before plugin configuration", function()
+        print "---------------- Test proxy access point before plugin configuration"
 
-    print"=============== Enable plugin for the Service"
-    local res, err = sh_until_ok(10,
-        [[curl --fail -i -sS -X POST  --url http://localhost:]], ctx.kong_admin_port,
-        [[/services/demo-service/plugins/  --data 'name=gluu-oauth2-client-auth' ]],
-        [[ --data "config.op_url=https://gluu-test.org" ]],
-        [[ --data "config.oxd_url=http://oxd-mock" ]],
-        [[ --data "config.allow_oauth_scope_expression=true" ]],
-        [[ --data "config.oauth_scope_expression=[{\"path\":\"/posts\",\"conditions\":[{\"httpMethods\":[\"GET\",\"DELETE\",\"POST\"],\"scope_expression\":{\"and\":[\"admin\",{\"not\":[\"employee\"]}]}}]}]" ]],
-        [[ --data "config.client_id=]], register_site_response.client_id, "\" ",
-        [[ --data "config.client_secret=]], register_site_response.client_secret, "\" ",
-        [[ --data "config.oxd_id=]], register_site_response.oxd_id, "\" "
-    )
+        it("Test it works", function()
+            local res, err = sh_until_ok(10, [[curl --fail -i -sS -X GET --url http://localhost:]],
+                ctx.kong_proxy_port, [[/ --header 'Host: backend.com']])
+        end)
+    end)
 
-    print"=============== Test it fail with 401 without token"
-    local res, err = sh_ex([[curl -i -sS -X GET --url http://localhost:]],
-        ctx.kong_proxy_port, [[/ --header 'Host: backend.com']])
-    assert(res:find("401"), 1, true)
+    describe("Test OAuth plugin configuration API", function()
+        print "---------------- Test OAuth plugin configuration API"
+        it("Test validation on plugin config", function()
+            local res, err = sh_ex([[curl -i -v -sS -X POST  --url http://localhost:]], ctx.kong_admin_port,
+                [[/services/demo-service/plugins/ --data 'name=gluu-oauth2-client-auth' ]],
+                [[ --data "config.op_url=stub" ]],
+                [[ --data "config.oxd_url=http://oxd-mock" ]],
+                [[ --data "config.anonymous=123-456" ]],
+                [[ --data "config.oauth_scope_expression=[{\"path\":\"/posts\",\"conditions\":[{\"httpMethods\":[\"GET\",\"DELETE\",\"POST\",\"scope_expression\":{\"and\":[\"admin\",{\"not\":[\"employee\"]}]}}]}]" ]],
+                [[ --data "config.client_id=]], register_site_response.client_id, "\" ",
+                [[ --data "config.client_secret=]], register_site_response.client_secret, "\" ",
+                [[ --data "config.oxd_id=]], register_site_response.oxd_id, "\" ")
+            assert(res:find("400"), 1, true)
+            JSON:decode(res)
+        end)
 
-    print"=============== Create a consumer"
-    local res, err = sh_until_ok(10,
-        [[curl --fail -v -sS -X POST --url http://localhost:]],
-        ctx.kong_admin_port, [[/consumers/ --data 'custom_id=]], register_site_response.client_id, [[']]
-    )
+        it("Enable plugin for the Service", function()
+            local res, err = sh_until_ok(10,
+                [[curl --fail -i -sS -X POST  --url http://localhost:]], ctx.kong_admin_port,
+                [[/services/demo-service/plugins/  --data 'name=gluu-oauth2-client-auth' ]],
+                [[ --data "config.op_url=https://gluu-test.org" ]],
+                [[ --data "config.oxd_url=http://oxd-mock" ]],
+                [[ --data "config.allow_oauth_scope_expression=true" ]],
+                [[ --data "config.oauth_scope_expression=[{\"path\":\"/posts\",\"conditions\":[{\"httpMethods\":[\"GET\",\"DELETE\",\"POST\"],\"scope_expression\":{\"and\":[\"admin\",{\"not\":[\"employee\"]}]}}]}]" ]],
+                [[ --data "config.client_id=]], register_site_response.client_id, "\" ",
+                [[ --data "config.client_secret=]], register_site_response.client_secret, "\" ",
+                [[ --data "config.oxd_id=]], register_site_response.oxd_id, "\" ")
+        end)
+    end)
 
-    local consumer_response = JSON:decode(res)
+    describe("Test proxy access point after plugin configuration", function()
+        print "---------------- Test proxy access point after plugin configuration"
 
-    print"=============== Test it work with token, consumer is registered"
-    local res, err = sh_ex(
-        [[curl --fail -i -sS  -X GET --url http://localhost:]], ctx.kong_proxy_port,
-        [[/ --header 'Host: backend.com' --header 'Authorization: Bearer ]],
-        access_token, [[']]
-    )
+        it("Test it fail with 401 without token", function()
+            local res, err = sh_ex([[curl -i -sS -X GET --url http://localhost:]],
+                ctx.kong_proxy_port, [[/ --header 'Host: backend.com']])
+            assert(res:find("401"), 1, true)
+        end)
 
-    -- backend returns all headrs within body
-    print"check that GG set all required upstream headers"
-    assert(res:lower():find("x-consumer-id: " .. string.lower(consumer_response.id), 1, true))
-    assert(res:lower():find("x-oauth-client-id: " .. string.lower(register_site_response.client_id), 1, true))
-    assert(res:lower():find("x-consumer-custom-id: " .. string.lower(register_site_response.client_id), 1, true))
-    assert(res:lower():find("x%-oauth%-expiration: %d+"))
+        it("Test it work with token, consumer is registered", function()
+            print "Create a consumer"
+            local res, err = sh_until_ok(10,
+                [[curl --fail -v -sS -X POST --url http://localhost:]],
+                ctx.kong_admin_port, [[/consumers/ --data 'custom_id=]], register_site_response.client_id, [[']])
+            local consumer_response = JSON:decode(res)
 
-    local res, err = sh_ex(
-        [[curl --fail -i -sS  -X GET --url http://localhost:]], ctx.kong_proxy_port,
-        [[/ --header 'Host: backend.com' --header 'Authorization: Bearer ]],
-        access_token, [[']]
-    )
+            local res, err = sh_ex(
+                [[curl --fail -i -sS  -X GET --url http://localhost:]], ctx.kong_proxy_port,
+                [[/ --header 'Host: backend.com' --header 'Authorization: Bearer ]],
+                access_token, [[']]
+            )
 
-    print"=============== Test it fail with 403 with wrong Bearer token"
-    local res, err = sh_ex(
-        [[curl -i -sS  -X GET --url http://localhost:]], ctx.kong_proxy_port,
-        [[/ --header 'Host: backend.com' --header 'Authorization: Bearer bla-bla']]
-    )
-    assert(res:find("403"))
+            -- backend returns all headrs within body
+            print"check that GG set all required upstream headers"
+            assert(res:lower():find("x-consumer-id: " .. string.lower(consumer_response.id), 1, true))
+            assert(res:lower():find("x-oauth-client-id: " .. string.lower(register_site_response.client_id), 1, true))
+            assert(res:lower():find("x-consumer-custom-id: " .. string.lower(register_site_response.client_id), 1, true))
+            assert(res:lower():find("x%-oauth%-expiration: %d+"))
 
-    print"=============== Test it works with the same token again, oxd-model id completed, token taken from cache"
-    local res, err = sh_ex(
-        [[curl --fail -i -sS  -X GET --url http://localhost:]], ctx.kong_proxy_port,
-        [[/ --header 'Host: backend.com' --header 'Authorization: Bearer ]],
-        access_token, [[']]
-    )
+            local res, err = sh_ex(
+                [[curl --fail -i -sS  -X GET --url http://localhost:]], ctx.kong_proxy_port,
+                [[/ --header 'Host: backend.com' --header 'Authorization: Bearer ]],
+                access_token, [[']]
+            )
+        end)
+
+        it("Test it fail with 401 with wrong Bearer token", function()
+            local res, err = sh_ex(
+                [[curl -i -sS  -X GET --url http://localhost:]], ctx.kong_proxy_port,
+                [[/ --header 'Host: backend.com' --header 'Authorization: Bearer bla-bla']]
+            )
+            assert(res:find("401"))
+        end)
+
+        it("Test it works with the same token again, oxd-model id completed, token taken from cache", function()
+            local res, err = sh_ex(
+                [[curl --fail -i -sS  -X GET --url http://localhost:]], ctx.kong_proxy_port,
+                [[/ --header 'Host: backend.com' --header 'Authorization: Bearer ]],
+                access_token, [[']]
+            )
+        end)
+    end)
 
     print_logs = false -- comment it out if want to see logs
 end)
