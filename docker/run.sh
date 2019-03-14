5c6c7f12a94f9040d9d66d525ed56869fb971e7e
David <nikdavnik@gmail.com>
2019-03-14 23:21:08 +0400

    Update run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -17,18 +17,22 @@ export CLIENT_ID=${CLIENT_ID:=""}
 export CLIENT_SECRET=${CLIENT_SECRET:=""}
 export LICENSE=${LICENSE:=""}
 
-sed -i "s/\$IP_ADDRESS/$IP_ADDRESS/g" setup-gluu-gateway_template.py
-sed -i "s/\$HOSTNAME/$HOSTNAME/g" setup-gluu-gateway_template.py
-sed -i "s/\$TWO_LETTER_COUNTRY_CODE/$TWO_LETTER_COUNTRY_CODE/g" setup-gluu-gateway_template.py
-sed -i "s/\$TWO_LETTER_STATE_CODE/$TWO_LETTER_STATE_CODE/g" setup-gluu-gateway_template.py
-sed -i "s/\$CITY_OR_LOCATION/$CITY_OR_LOCATION/g" setup-gluu-gateway_template.py
-sed -i "s/\$ORGANIZATION_NAME/$ORGANIZATION_NAME/g" setup-gluu-gateway_template.py
-sed -i "s/\$EMAIL_ADDRESS/$EMAIL_ADDRESS/g" setup-gluu-gateway_template.py
-sed -i "s/\$PGSQL_PASSWORD/$PGSQL_PASSWORD/g" setup-gluu-gateway_template.py
-sed -i "s/\$OP_HOST/$OP_HOST/g" setup-gluu-gateway_template.py
-sed -i "s/\$OXD_SERVER_URL/$OXD_SERVER_URL/g" setup-gluu-gateway_template.py
-sed -i "s/\$GENERATE_CLIENT_CREDS_FOR_OXD/$GENERATE_CLIENT_CREDS_FOR_OXD/g" setup-gluu-gateway_template.py
-sed -i "s/\$OXD_ID/$OXD_ID/g" setup-gluu-gateway_template.py
-sed -i "s/\$CLIENT_ID/$CLIENT_ID/g" setup-gluu-gateway_template.py
-sed -i "s/\$CLIENT_SECRET/$CLIENT_SECRET/g" setup-gluu-gateway_template.py
-sed -i "s/\$LICENSE/$LICENSE/g" setup-gluu-gateway_template.py
+sed -i "s/\$IP_ADDRESS/$IP_ADDRESS/g" /setup-gluu-gateway_template.py
+sed -i "s/\$HOSTNAME/$HOSTNAME/g" /setup-gluu-gateway_template.py
+sed -i "s/\$TWO_LETTER_COUNTRY_CODE/$TWO_LETTER_COUNTRY_CODE/g" /setup-gluu-gateway_template.py
+sed -i "s/\$TWO_LETTER_STATE_CODE/$TWO_LETTER_STATE_CODE/g" /setup-gluu-gateway_template.py
+sed -i "s/\$CITY_OR_LOCATION/$CITY_OR_LOCATION/g" /setup-gluu-gateway_template.py
+sed -i "s/\$ORGANIZATION_NAME/$ORGANIZATION_NAME/g" /setup-gluu-gateway_template.py
+sed -i "s/\$EMAIL_ADDRESS/$EMAIL_ADDRESS/g" /setup-gluu-gateway_template.py
+sed -i "s/\$PGSQL_PASSWORD/$PGSQL_PASSWORD/g" /setup-gluu-gateway_template.py
+sed -i "s/\$OP_HOST/$OP_HOST/g" /setup-gluu-gateway_template.py
+sed -i "s/\$OXD_SERVER_URL/$OXD_SERVER_URL/g" /setup-gluu-gateway_template.py
+sed -i "s/\$GENERATE_CLIENT_CREDS_FOR_OXD/$GENERATE_CLIENT_CREDS_FOR_OXD/g" /setup-gluu-gateway_template.py
+sed -i "s/\$OXD_ID/$OXD_ID/g" /setup-gluu-gateway_template.py
+sed -i "s/\$CLIENT_ID/$CLIENT_ID/g" /setup-gluu-gateway_template.py
+sed -i "s/\$CLIENT_SECRET/$CLIENT_SECRET/g" /setup-gluu-gateway_template.py
+sed -i "s/\$LICENSE/$LICENSE/g" /setup-gluu-gateway_template.py
+
+cat /setup-gluu-gateway_template.py > /opt/gluu-gateway/setup/setup-gluu-gateway.py
+cd /opt/gluu-gateway/setup/
+python setup-gluu-gateway.py
