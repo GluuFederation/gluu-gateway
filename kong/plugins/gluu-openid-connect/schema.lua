9a63491924e4e705c0e6e54c1af76cfdd339d746
Meghna Joshi <ldeveloperl1985@gmail.com>
2019-03-28 18:05:25 +0530

    #283: Small fixed: Store array values for required_acrs

--- kong/plugins/gluu-openid-connect/schema.lua
+++ kong/plugins/gluu-openid-connect/schema.lua
@@ -10,7 +10,7 @@ return {
         logout_path = { required = false, type = "string" },
         post_logout_redirect_uri = { required = false, type = "string" }, --TODO must be registered as well as authorization_redirect_uri
         requested_scopes = {required = true, type = "array"},
-        required_acrs = {required = false, type = "string"}, -- UI should show associated levels
+        required_acrs = {required = false, type = "array"}, -- UI should show associated levels
         max_id_token_age = { required = true, type = "timestamp"},
         max_id_token_auth_age = { required = true, type = "timestamp"},
         -- headers - at the moment we will include some hardcoded header set
