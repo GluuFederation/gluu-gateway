2d178f7c48482caf75f37b7a2ad40fcb94ca7bff
Meghna Joshi <ldeveloperl1985@gmail.com>
2019-07-22 12:34:38 +0530

    #319: Update oxd package name

--- setup/setup-gluu-gateway.py
+++ setup/setup-gluu-gateway.py
@@ -158,9 +158,9 @@ class KongSetup(object):
         self.prometheus_file_path = '%s/third-party/nginx-lua-prometheus/prometheus.lua' % self.dist_gluu_gateway_folder
 
         # oxd file names
-        self.ubuntu16_oxd_file = "oxd-server_4.0-68~xenial+Ub16.04_all.deb"
-        self.centos7_oxd_file = "oxd-server-4.0-68.centos7.noarch.rpm"
-        self.rhel7_oxd_file = "oxd-server-4.0-68.rhel7.noarch.rpm"
+        self.ubuntu16_oxd_file = "oxd-server_4.0-70-beta1~xenial+Ub16.04_all.deb"
+        self.centos7_oxd_file = "oxd-server-4.0-70-beta1.centos7.noarch.rpm"
+        self.rhel7_oxd_file = "oxd-server-4.0-70-beta1.rhel7.noarch.rpm"
         self.oxd_log_format = "%-6level [%d{HH:mm:ss.SSS}] [%t] %logger{5} - %X{code} %msg %n"
         self.oxd_archived_log_filename_pattern = "/var/log/oxd-server/oxd-server-%d{yyyy-MM-dd}-%i.log.gz"
 
