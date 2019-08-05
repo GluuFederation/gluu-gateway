3e74e1f2e3350928b29b183f263bf492dd415cfa
Alexander Altshuler <altexy@gmail.com>
2019-08-05 16:19:49 +0300

    add required_acrs per URI, refactor URI matching, #345

--- kong/plugins/gluu-oauth-pep/schema.lua
+++ kong/plugins/gluu-oauth-pep/schema.lua
@@ -1,4 +1,3 @@
-local path_wildcard_tree = require "gluu.path-wildcard-tree"
 local common = require "gluu.kong-common"
 
 return {
@@ -14,21 +13,7 @@ return {
         method_path_tree = { required = false, type = "table" },
     },
     self_check = function(schema, plugin_t, dao, is_updating)
-        local method_path_tree = {}
-        local oauth_scope_expression = plugin_t.oauth_scope_expression
-        for k = 1, #oauth_scope_expression do
-            local item = oauth_scope_expression[k]
-
-            for i = 1, #item.conditions do
-                local condition = item.conditions[i]
-
-                for j = 1, #condition.httpMethods do
-                    local t = { path = item.path, scope_expression = condition.scope_expression }
-                    path_wildcard_tree.addPath(method_path_tree, condition.httpMethods[j], item.path, t)
-                end
-            end
-        end
-        plugin_t.method_path_tree = method_path_tree
+        plugin_t.method_path_tree = common.convert_scope_expression_to_path_wildcard_tree(plugin_t.oauth_scope_expression)
         return true
     end
 }
