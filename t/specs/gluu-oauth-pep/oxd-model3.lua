1bb1c6949d7781bee3c006bb2ea29a0d4c0428cb
Alexander Altshuler <altexy@gmail.com>
2020-02-13 16:35:37 +0300

    test: add cases for the #423

--- t/specs/gluu-oauth-pep/oxd-model3.lua
+++ t/specs/gluu-oauth-pep/oxd-model3.lua
@@ -92,6 +92,36 @@ model = {
             response.iat = ngx.now()
         end,
     },
+    -- #5, plugin check the client token and scope with scope_expression for path /posts
+    {
+        expect = "/introspect-access-token",
+        required_fields = {
+            "oxd_id",
+            "access_token",
+        },
+        request_check = function(json, token)
+            assert(json.oxd_id == model[1].response.oxd_id)
+            assert(json.access_token == model[2].response.access_token)
+            assert(token == model[3].response.access_token, 403)
+        end,
+        response = {
+            active = true,
+            client_id = "@!1736.179E.AA60.16B2!0001!8F7C.B9AB!0008!A2BB.9AE6.5F14.B387", -- should be the same as return by register-site
+            username = "John Black",
+            scope = { "admin", "employee" },
+            token_type = "bearer",
+            sub = "jblack",
+            aud = "l238j323ds-23ij4",
+            iss = "https://as.gluu.org/",
+            --acr_values": ["basic","duo"],
+            --extension_field": "twenty-seven",
+        },
+        response_callback = function(response)
+            response.exp = ngx.now() + 60 * 60
+            response.iat = ngx.now()
+        end,
+    },
+
 }
 
 return model
